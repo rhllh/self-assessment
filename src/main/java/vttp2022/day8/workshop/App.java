6e2e1ad18d6142569aaa52621c2a547390de45aa
Rahillah <rahillah.yusri@gmail.com>
2022-06-22 13:00:25 +0800

    Initial commit

---
+++ src/main/java/vttp2022/day8/workshop/App.java
@@ -0,0 +1,13 @@
+package vttp2022.day8.workshop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
